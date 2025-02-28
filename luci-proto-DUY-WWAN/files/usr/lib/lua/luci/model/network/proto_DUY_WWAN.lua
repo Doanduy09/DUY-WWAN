e6e3482b080285b7d3378318afe472c3511bb44e
Doanduy09 <doanbangduy1986@gmail.com>
2025-02-28 21:10:04 +0700

    Enhance luci-proto-DUY-WWAN with advanced features

--- luci-proto-DUY-WWAN/files/usr/lib/lua/luci/model/network/proto_DUY_WWAN.lua
+++ luci-proto-DUY-WWAN/files/usr/lib/lua/luci/model/network/proto_DUY_WWAN.lua
@@ -1,11 +1,1 @@
-local m = require "luci.model.network".inherit("DUY_WWAN")
-m.name = "DUY-WWAN"
-m.description = "DUY-WWAN Protocol for 4G/5G"
-function m.setup(self, ifname)
-    self:ifname(ifname)
-    self:set("proto", "DUY_WWAN")
-end
-function m.teardown(self)
-    self:del("proto")
-end
-return m
+[copy nội dung proto_DUY_WWAN.lua từ cải tiến trước vào đây]
